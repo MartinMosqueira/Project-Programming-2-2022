fe5c4abd6efe4984d1f92e49c24532be39e59ade
MartinMosqueira <tinchomosqueira@gmail.com>
2022-07-20 10:29:37 -0300

    Finished configuration DB
    The necessary configurations were added to the "application.properties" file to connect to the local postgresql DB.

--- FranchiseMicroservice/src/main/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplication.java
+++ FranchiseMicroservice/src/main/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplication.java
@@ -2,9 +2,8 @@ package app.project.FranchiseMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class FranchiseMicroserviceApplication {
 
 	public static void main(String[] args) {
