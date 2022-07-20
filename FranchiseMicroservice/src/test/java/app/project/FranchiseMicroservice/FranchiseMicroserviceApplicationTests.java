e49ce41f1e68cfb08cb0f3d0e41ecb1c246b4e27
MartinMosqueira <tinchomosqueira@gmail.com>
2022-07-20 09:20:35 -0300

    Initial Structure

---
+++ FranchiseMicroservice/src/test/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.project.FranchiseMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
