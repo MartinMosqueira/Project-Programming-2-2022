dc51514c7023612f3008465c07a7f9e5305fa887
MartinMosqueira <tinchomosqueira@gmail.com>
2022-08-06 14:08:10 -0300

    Connected second microservice at Consul

--- ReportsService/src/main/java/app/poject/ReportsService/ReportsServiceApplication.java
+++ ReportsService/src/main/java/app/poject/ReportsService/ReportsServiceApplication.java
@@ -2,9 +2,24 @@ package app.poject.ReportsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@Controller
 public class ReportsServiceApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home(HttpServletRequest request){
+		StringBuilder builder=new StringBuilder();
+		builder.append("<h1>Service Reports</h1>");
+
+		return builder.toString();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportsServiceApplication.class, args);
