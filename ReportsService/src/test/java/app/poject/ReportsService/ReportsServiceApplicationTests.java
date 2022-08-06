2939fab36d170b32de7143c07c093e2401d92729
MartinMosqueira <tinchomosqueira@gmail.com>
2022-08-06 13:55:25 -0300

    Initial structure second reporting microservice

---
+++ ReportsService/src/test/java/app/poject/ReportsService/ReportsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.poject.ReportsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
