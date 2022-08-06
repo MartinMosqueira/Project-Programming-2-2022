a2e9048e5173fcac8eb425cf65c1fcd63c1c4296
MartinMosqueira <tinchomosqueira@gmail.com>
2022-08-06 13:09:20 -0300

    Franchise microservice connected to Consul
     The franchising microservice is registered to consul.

--- FranchiseMicroservice/src/main/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplication.java
+++ FranchiseMicroservice/src/main/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplication.java
@@ -1,10 +1,27 @@
 package app.project.FranchiseMicroservice;
 
+import org.apache.http.impl.bootstrap.HttpServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@Controller
 public class FranchiseMicroserviceApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home(HttpServletRequest request){
+		StringBuilder builder=new StringBuilder();
+		builder.append("<h1>Service Franchise</h1>");
+
+		return builder.toString();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FranchiseMicroserviceApplication.class, args);
