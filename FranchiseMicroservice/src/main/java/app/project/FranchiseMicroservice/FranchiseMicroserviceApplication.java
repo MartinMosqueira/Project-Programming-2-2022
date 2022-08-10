9cc4857f98209ae77def15fbd12d58b1a712172c
MartinMosqueira <tinchomosqueira@gmail.com>
2022-08-09 21:10:06 -0300

    Test microservice Reports
    Tested the connection to the discovery service and the reporting microservice

--- FranchiseMicroservice/src/main/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplication.java
+++ FranchiseMicroservice/src/main/java/app/project/FranchiseMicroservice/FranchiseMicroserviceApplication.java
@@ -3,6 +3,7 @@ package app.project.FranchiseMicroservice;
 import org.apache.http.impl.bootstrap.HttpServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Controller
 public class FranchiseMicroserviceApplication {
 
--- ReportsService/src/main/java/app/poject/ReportsService/ReportsServiceApplication.java
+++ ReportsService/src/main/java/app/poject/ReportsService/ReportsServiceApplication.java
@@ -2,6 +2,7 @@ package app.poject.ReportsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Controller
 public class ReportsServiceApplication {
 
